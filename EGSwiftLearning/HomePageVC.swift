d58da43ead569bce539e551ce8ce6b23d2dfd5f2
yz <liuyz@powerinfo.net>
2019-10-17 09:55:56 +0800

    define RootVC
    define RootVC

---
+++ EGSwiftLearning/HomePageVC.swift
@@ -0,0 +1,30 @@
+//
+//  HomePageVC.swift
+//  EGSwiftLearning
+//
+//  Created by Easer Liu on 2019/10/17.
+//  Copyright © 2019 Easer. All rights reserved.
+//
+
+import UIKit
+
+class HomePageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.green
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
